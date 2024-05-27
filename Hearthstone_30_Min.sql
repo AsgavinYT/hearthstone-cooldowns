3e59177c2ea6c04e703601d3dfb8d781bd9fe4a3
Asgavin <99362152+AsgavinYT@users.noreply.github.com>
2024-05-27 16:59:44 +0200

    Add files via upload

---
+++ Hearthstone_30_Min.sql
@@ -0,0 +1,2 @@
+UPDATE item_template SET spellcooldown_1=-1 WHERE spellcooldown_1 BETWEEN 1 AND 900000 AND entry=6948;
+UPDATE item_template SET spellcategorycooldown_1=-1 WHERE spellcategorycooldown_1 BETWEEN 1 AND 900000 AND entry=6948;
---
+++ Hearthstone_5_Min.sql
@@ -0,0 +1,2 @@
+UPDATE item_template SET spellcooldown_1=300000 WHERE spellcooldown_1=-1 AND entry=6948;
+UPDATE item_template SET spellcategorycooldown_1=300000 WHERE spellcategorycooldown_1=-1 AND entry=6948;
